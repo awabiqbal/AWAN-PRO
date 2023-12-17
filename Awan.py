c72fa9b590c7620b3ff222be0fef34ab36264201
AWAN-XD <145342995+AWAN-143@users.noreply.github.com>
2023-12-17 14:17:46 +0500

    Update Awan.py

--- Awan.py
+++ Awan.py
@@ -3,4 +3,5 @@ import time
 os.system("git pull")
 print('updating (81.5)...');time.sleep (1.1)
 os.system("xdg-open https://chat.whatsapp.com/LVHHgKJW6EQHb20n6R4Du8")
-import AWNNEW
+print(' wait for update')
+exit()
